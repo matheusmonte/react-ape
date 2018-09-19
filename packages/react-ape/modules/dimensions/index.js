8f1b57c07bc73335c1f49a0f6d5cdaf024ebed20
Matheus Clark dos Santos Monte <matheus.m@samsung.com>
2018-09-19 12:59:01 -0400

    Add windows events on Dimensions
    Signed-off-by: Matheus Clark dos Santos Monte <matheus.m@samsung.com>

--- packages/react-ape/modules/dimensions/index.js
+++ packages/react-ape/modules/dimensions/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function getWidth() {
   if (window && window.innerWidth) {
     return window.innerWidth;
@@ -10,17 +12,20 @@ function getHeight() {
   }
 }
 
-function addEventListener(){
-
+function addEventListener(type: string, handler: Function) {
+  if (type === undefined) {
+    return 'You are trying to add a undefinied event';
+  }
+  window.addEventListener(type, handler);
 }
 
-function removeEventListener(){
-
+function removeEventListener(type: string, handler: Function) {
+  window.removeEventListener(type, handler);
 }
 
 export default {
   getWidth,
   getHeight,
   addEventListener,
-  removeEventListener
+  removeEventListener,
 };
